2377443cafbed854349538591e63bd789b4aefd5
Miguel Luna García <miguel00123456@gmail.com>
2023-11-30 18:58:39 +0000

    Subida de archivos

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,19 +4,68 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    enum MoleType {
+        Easy,
+        Medium,
+        Hard,
+        Bad
+    }
     public TextMesh scoreText;
     public int score;
+    private float easyMoleProb;
+    private float mediumMoleProb;
+    private float hardMoleProb;
+    private GameObject[] pipes;
+    public float interval = 1f;
+    private float time;
+    private int numActivePipes ;
+
     void Start()
     {
         score = 0;
+        numActivePipes = 0;
         scoreText.text = "SCORE\n000";
+        pipes = GameObject.FindGameObjectsWithTag("MolePipe");
+        time = 0f;
+
+        easyMoleProb = 1/2f;
+        mediumMoleProb = easyMoleProb + 1/4f;
+        hardMoleProb = mediumMoleProb + 1/8f;
     }
 
-    
     void Update()
     {
         scoreText.text = "SCORE\n" + score.ToString("000");
-        score++;
-        Debug.Log(score.ToString("000"));
+        time += Time.deltaTime;
+        
+        if (time > interval) {
+            time = 0f;
+            GameObject pipe = SelectPipe();
+            MoleType moleType = SelectMoleType();
+            pipe.GetComponent<PipeController>().ActivateMole();
+            numActivePipes++;
+        }
+
+    }
+
+    MoleType SelectMoleType() {
+        float prob = Random.Range(0f, 1f);
+        if (prob < easyMoleProb) {
+            return MoleType.Easy;
+        } else if (prob < mediumMoleProb) {
+            return MoleType.Medium;
+        } else if (prob < hardMoleProb) {
+            return MoleType.Hard;
+        } else {
+            return MoleType.Bad;
+        }
+    }
+
+    GameObject SelectPipe() {
+        int index = Random.Range(0, pipes.Length);
+        do {
+            index = Random.Range(0, pipes.Length);
+        } while (pipes[index].GetComponent<PipeController>().IsMoleActive() && numActivePipes < pipes.Length);
+        return pipes[index];
     }
 }
--- Assets/Scripts/PipeController.cs
+++ Assets/Scripts/PipeController.cs
@@ -2,8 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PipeController : MonoBehaviour
 {
+    enum MoleType {
+        Easy,
+        Medium,
+        Hard,
+        Bad
+    }
+    public bool isActive;
+    public GameObject mole;
+    public float speed = 5f;
+    private float timeActive = 0f;
+    void Start() {
+        isActive = false;
+        mole.transform.localPosition = new Vector3(0, 0, 0);
+    }
+
+    void Update() {
+        if (isActive && mole.transform.localPosition.y < 1f) {
+            mole.transform.Translate(Vector3.up * Time.deltaTime * speed);
+        } else if (!isActive && mole.transform.localPosition.y > 0f) {
+            mole.transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+        timeActive += Time.deltaTime;
+    }
+
+    public void ActivateMole() { 
+        isActive = true;
+        timeActive = 0f;
+    }
+
+    public void DeactivateMole() {
+        isActive = false;
+    }
+
+    public bool IsMoleActive() {
+        return isActive;
+    }
+
     void OnPointerEnter() {
         Debug.Log("Mouse enter");
     }
